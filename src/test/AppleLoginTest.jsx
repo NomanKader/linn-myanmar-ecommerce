390ea6eb8b50ba690f54f88c5987c94596f41891
NomanKader <nomankader786@gmail.com>
2024-03-24 00:22:38 +0630

    Apple Login Test

--- src/test/AppleLoginTest.jsx
+++ src/test/AppleLoginTest.jsx
@@ -3,11 +3,12 @@ import AppleSignin from 'react-apple-signin-auth';
 import axios from 'axios'; // Import axios for making HTTP requests
 //import .p8 file which is under src folder
 import SecretFile from '../key/AuthKey_32C9B8T2BV.p8'
-const fs = require('fs');
+const fs = require('fs').promises;
 const jwt = require('jsonwebtoken');
 
 // Function to generate client_secret using id_token and private key
 const generateClientSecret = (idToken) => {
+  console.log("ID token",idToken);
   // Read the private key from the .p8 file
   const privateKey = fs.readFile(SecretFile,'utf8').toString();
 
