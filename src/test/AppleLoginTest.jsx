66fc1cd710b2b512c1ad1df9cc88502251fd5d0c
NomanKader <nomankader786@gmail.com>
2024-03-23 23:27:08 +0630

    Apple Login Test

--- src/test/AppleLoginTest.jsx
+++ src/test/AppleLoginTest.jsx
@@ -22,23 +22,7 @@ const AppleLoginTest = ({ ...rest }) => (
     /** Spread rest props if needed */
     {...rest}
     /** Callbacks */
-    onSuccess={async (response) => {
-      const authorizationCode = response.authorization.code;
-      // Now make a request to your server to exchange authorizationCode for user info
-      try {
-        const userInfoResponse = await fetch('/fetch-user-info', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ authorizationCode }),
-        });
-        const userInfo = await userInfoResponse.json();
-        console.log('User Info:', userInfo);
-      } catch (error) {
-        console.error('Error fetching user info:', error);
-      }
-    }}
+   onSuccess={(response) => console.log(response)}
     onFailure={(error) => console.log(error)}
     onError={(error) => console.error(error)}
   />
